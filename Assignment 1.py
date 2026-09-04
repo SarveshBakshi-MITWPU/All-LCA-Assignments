92f89e11bb690fc54892ee8d22e84a9dc850ed0a
SarveshBakshi-MITWPU <1272261166.s@mitwpu.edu.in>
2026-09-04 14:38:27 +0530

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -5,18 +5,14 @@ students = {
 }
 roll_no = [101, 102, 103]
 
-# Add a new record
 students[104] = ("Natasha", "IT", 85)
 roll_no.append(104)
 
-# Delete a record
 del students[102]
 roll_no.remove(102)
 
-# Update an existing record
 students[103] = ("Bruce", "AIML", 94)
 
-# Print the updated records with corrected loop variable name and proper indentation
 print("Updated Record of Students:")
 for roll, i in students.items():
     print("Roll Number: ", roll)
